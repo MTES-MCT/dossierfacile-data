1757b51564bd028b211b3e8f42a7186ae8176a2c
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-01-28 11:37:31 +0100

    remove property stagging and with table for tenant dates

--- models/core/core_tenant_account.sql
+++ models/core/core_tenant_account.sql
@@ -1,3 +1,17 @@
+with tenant_dates as ( -- récupération des dates pour chaque tenant
+    select
+        staging_tenant_log.tenant_id
+        , MIN(case when staging_tenant_log.log_type in ('ACCOUNT_CREATED', 'ACCOUNT_CREATED_VIA_KC', 'FC_ACCOUNT_CREATION') then staging_tenant_log.creation_date end) as creation_date
+        , MIN(case when staging_tenant_log.log_type in ('ACCOUNT_COMPLETED', 'ACCOUNT_VALIDATED', 'ACCOUNT_DENIED') then staging_tenant_log.creation_date end) as completion_date
+        , MAX(case when staging_tenant_log.log_type in ('ACCOUNT_COMPLETED', 'ACCOUNT_VALIDATED', 'ACCOUNT_DENIED') then 1 else 0 end) as completion_flag
+        , MIN(case when staging_tenant_log.log_type in ('ACCOUNT_VALIDATED', 'ACCOUNT_DENIED') then staging_tenant_log.creation_date end) as firstoperation_date
+        , MIN(case when staging_tenant_log.log_type = 'ACCOUNT_VALIDATED' then staging_tenant_log.creation_date end) as validation_date
+        , MAX(case when staging_tenant_log.log_type = 'ACCOUNT_VALIDATED' then 1 else 0 end) as validation_flag
+    from {{ ref('staging_tenant_log') }} as staging_tenant_log
+    group by
+        staging_tenant_log.tenant_id
+)
+
 select
     staging_tenant_log.tenant_id as id
     , apartment_sharing_id
@@ -13,13 +27,13 @@ select
     , france_connect_birth_place
     , france_connect_birth_country
     , staging_tenant.status
-    , core_tenant_dates.creation_date
+    , tenant_dates.creation_date
     , completion_date
     , completion_flag
     , firstoperation_date
     , validation_date
     , validation_flag
 from {{ ref('staging_tenant_log') }} as staging_tenant_log
-inner join {{ ref('core_tenant_dates') }} on core_tenant_dates.tenant_id = staging_tenant_log.tenant_id
+inner join tenant_dates on staging_tenant_log.tenant_id = tenant_dates.tenant_id
 left join {{ ref('staging_user_account') }} on staging_tenant_log.tenant_id = {{ ref('staging_user_account') }}.id
 left join {{ ref('staging_tenant') }} on staging_tenant_log.tenant_id = {{ ref('staging_tenant') }}.id
