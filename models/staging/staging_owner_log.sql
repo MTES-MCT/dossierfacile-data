c2b0a4ec614da1d832c8d464f824a156dd7e7210
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-02-10 14:49:10 +0100

    commenting staging_owner_log.sql

--- models/staging/staging_owner_log.sql
+++ models/staging/staging_owner_log.sql
@@ -2,5 +2,5 @@ select
     CAST(id as INTEGER) as id
     , CAST(owner_id as INTEGER) as owner_id
     , CAST(creation_date as TIMESTAMP) as creation_date
-    , CAST(log_type as VARCHAR) as log_type
+    , CAST(log_type as VARCHAR) as log_type                 -- la table staging_owner_log_json met a plat les data des json de suppression des comptes
 from owner_log
