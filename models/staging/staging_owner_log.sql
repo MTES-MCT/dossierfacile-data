6807bb6ff8fb5bcea1b4c8af28fac23480e9bbeb
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-02-11 17:27:44 +0100

    cleaning json_profile owner

--- models/staging/staging_owner_log.sql
+++ models/staging/staging_owner_log.sql
@@ -3,4 +3,4 @@ select
     , CAST(owner_id as INTEGER) as owner_id
     , CAST(creation_date as TIMESTAMP) as creation_date
     , CAST(log_type as VARCHAR) as log_type                 -- la table staging_owner_log_json met a plat les data des json de suppression des comptes
-from owner_log
+from {{ source('dossierfacile', 'owner_log') }}
--- models/staging/staging_owner_log_json.sql
+++ models/staging/staging_owner_log_json.sql
@@ -1,20 +1,66 @@
+with json_details as (
+    select
+        id
+        , json_profile ->> 'id' as owner_id
+        , json_profile ->> 'creationDateTime' as owner_creation_date        -- date de creation du compte du compte proprietaire
+        , json_profile ->> 'franceConnect' as owner_france_connect
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'id' as property_id
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'name' as property_name
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'type' as property_type
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'address' as property_address
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'rentCost' as property_rent_cost
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'furniture' as property_furniture
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'validated' as property_validated
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'chargesCost' as property_charges_cost
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'co2emission' as property_co2_emission
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'livingSpace' as property_living_space
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'energyConsumption' as property_energy_consumption
+        , JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'propertiesApartmentSharing' as property_apartment_sharing
+    from {{ source('dossierfacile', 'owner_log') }}
+    where json_profile is not null
+)
+
+, casting_logs as (
+    select
+        CAST(owner_log.id as INTEGER) as id
+        , CAST(owner_log.owner_id as INTEGER) as owner_id
+        , CAST(owner_log.creation_date as VARCHAR) as deletion_date
+        , CAST(owner_log.log_type as VARCHAR) as log_type
+        , CAST(json_details.owner_creation_date as VARCHAR) as owner_creation_date
+        , CAST(json_details.owner_france_connect as BOOLEAN) as owner_france_connect
+        , CAST(json_details.property_id as INTEGER) as property_id
+        , CAST(json_details.property_name as VARCHAR) as property_name
+        , CAST(json_details.property_type as VARCHAR) as property_type
+        , CAST(json_details.property_address as VARCHAR) as property_address
+        , CAST(json_details.property_rent_cost as VARCHAR) as property_rent_cost
+        , CAST(json_details.property_furniture as VARCHAR) as property_furniture
+        , CAST(json_details.property_validated as BOOLEAN) as property_validated
+        , CAST(json_details.property_charges_cost as FLOAT) as property_charges_cost
+        , CAST(json_details.property_co2_emission as FLOAT) as property_co2_emission
+        , CAST(json_details.property_living_space as FLOAT) as property_living_space
+        , CAST(json_details.property_energy_consumption as FLOAT) as property_energy_consumption
+        , CAST(json_details.property_apartment_sharing as VARCHAR) as property_apartment_sharing
+    from {{ source('dossierfacile', 'owner_log') }} as owner_log
+    inner join json_details on owner_log.id = json_details.id
+)
+
 select
-    CAST(id as INTEGER) as id
-    , CAST(json_profile ->> 'id' as INTEGER) as owner_id
-    , CAST(json_profile -> 'creationDateTime' as VARCHAR) as owner_creation_date        -- date de creation du compte du compte proprietaire
-    , CAST(json_profile -> 'franceConnect' as VARCHAR) as owner_france_connect
-    , CAST(creation_date as TIMESTAMP) as creation_date                                 -- date du log de suppression du compte proprietaire 
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'id' as INTEGER) as property_id
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'name' as VARCHAR) as property_name
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'type' as VARCHAR) as property_type
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'address' as VARCHAR) as property_address
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'rentCost' as VARCHAR) as property_rent_cost
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'furniture' as VARCHAR) as property_furniture
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'validated' as BOOLEAN) as property_validated
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'chargesCost' as FLOAT) as property_charges_cost
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'co2emission' as FLOAT) as property_co2_emission
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'livingSpace' as FLOAT) as property_living_space
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'energyConsumption' as FLOAT) as property_energy_consumption
-    , CAST(JSONB_ARRAY_ELEMENTS(json_profile -> 'properties') ->> 'propertiesApartmentSharing' as VARCHAR) as property_apartment_sharing
-from owner_log
-where json_profile is not null
+    id
+    , owner_id
+    , deletion_date
+    , log_type
+    , owner_creation_date
+    , owner_france_connect
+    , property_id
+    , property_name
+    , property_type
+    , property_address
+    , property_rent_cost
+    , property_furniture
+    , property_validated
+    , property_charges_cost
+    , property_co2_emission
+    , property_living_space
+    , property_energy_consumption
+    , property_apartment_sharing
+from casting_logs
