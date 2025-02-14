e7b9ef05d33bfb08df66a084ae4cc2754ff28a42
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-02-14 16:10:05 +0100

    feat: update analytics model for tenant

--- models/analytics/analytics_daily_tenant_account.sql
+++ models/analytics/analytics_daily_tenant_account.sql
@@ -1,6 +1,22 @@
 select
     DATE(created_at) as created_date
-    , COUNT(*) as nb_creation
+    , tenant_origin
+    , tenant_type
+    , COUNT(id) as nb_creation
+    , COUNT(is_france_connected) as nb_france_connected
+    , COUNT(completion_flag) as nb_account_completions
+    , COUNT(validation_flag) as nb_account_validations
+    , SUM(time_to_completion) as total_time_to_completion
+    , SUM(time_to_validation) as total_time_to_validation
+    , SUM(nb_completions) as total_completions
+    , SUM(nb_operations) as total_operations
+    , SUM(nb_validations) as total_validations
 from {{ ref('core_tenant_account') }}
 group by
     DATE(created_at)
+    , tenant_origin
+    , tenant_type
+order by 
+    DATE(created_at) desc
+    , tenant_origin
+    , tenant_type
