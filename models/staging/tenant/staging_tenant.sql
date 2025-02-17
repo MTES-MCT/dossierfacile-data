4cc07a96983ec4ecd15eaab3fc2d15d46cdb5318
Romaric <Rhmaric@users.noreply.github.com>
2025-02-17 16:07:05 +0100

    fix: core_tenant status not null test (#10)

--- models/staging/tenant/staging_tenant.sql
+++ models/staging/tenant/staging_tenant.sql
@@ -6,7 +6,7 @@ select
     , CAST(honor_declaration as BOOLEAN)
     , CAST(last_update_date as TIMESTAMP) as last_updated_at
     , CAST(clarification as VARCHAR) as tenant_comment -- colonne clarification de tenant. c'est le commentaire de l utilisateur à destination du proprietaire
-    , CAST(status as VARCHAR)
+    , CAST(COALESCE(status, 'INCOMPLETE') as VARCHAR) as status
     , CAST(operator_date_time as TIMESTAMP) as last_operation_at
     , CAST(warnings as INTEGER) as deletion_warnings
     , CAST(operator_comment as VARCHAR)
