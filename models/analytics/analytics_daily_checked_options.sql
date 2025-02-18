dd99780eab9b3e36f2dd65dbe3bc232331c74583
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-02-18 18:23:36 +0100

    feat: remove funnel_type, it ll be in the core_tenant_account next version

--- models/analytics/analytics_daily_checked_options.sql
+++ models/analytics/analytics_daily_checked_options.sql
@@ -4,7 +4,6 @@ select
     , checked_options
     , document_user_type
     , tenant_origin
-    , case when tenant_origin ilike '%hybrid%' then 'partner' else 'organic' end as funnel_type
     , DATE(document_denied_at) as document_denied_at
     , COUNT(*) as nb_checked_options
 from {{ ref('core_document_denied') }}
@@ -15,4 +14,3 @@ group by
     , checked_options
     , document_user_type
     , tenant_origin
-    , case when tenant_origin ilike '%hybrid%' then 'partner' else 'organic' end
