c833714507c5517ccbcdeaa7eada7dda611752e8
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-03-21 11:02:23 +0100

    feat: change weekly by monthly analysis for property creations

--- models/analytics/analytics_monthly_property_creations.sql
+++ models/analytics/analytics_monthly_property_creations.sql
@@ -10,9 +10,10 @@ with property_creations as (
 )
 
 select
-    DATE_TRUNC('week', created_at) as created_at
+    DATE_TRUNC('month', created_at) as created_at
     , SUM(case when row_number = 1 then 1 else 0 end) as first_properties
     , SUM(case when row_number > 1 then 1 else 0 end) as multiple_properties
 from property_creations
 where row_number = 1
-group by 1
+group by 
+DATE_TRUNC('month', created_at)
