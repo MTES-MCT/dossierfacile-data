c47db9dfe7815138c1133c482a1bc3ac1e606556
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-03-21 11:16:20 +0100

    feat: add tenant_origin in the monthly_partner_account

--- models/analytics/analytics_monthly_partner_accounts.sql
+++ models/analytics/analytics_monthly_partner_accounts.sql
@@ -1,5 +1,6 @@
 select
     DATE_TRUNC('month', created_at) as created_at
+    , tenant_origin
     , COUNT(*) as nb_creations
     , SUM(completion_flag) as nb_completions
     , SUM(validation_flag) as nb_validations
@@ -7,3 +8,4 @@ from {{ ref('core_tenant_account') }}
 where tenant_origin in ('hybrid-pap', 'dfconnect-locservice', 'dfconnect-jinka', 'dfconnect-immojeune')
 group by
     DATE_TRUNC('month', created_at)
+    , tenant_origin
