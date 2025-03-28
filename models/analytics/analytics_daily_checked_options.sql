be69807cd8edc522c0554eb07ce22a2131ee87bf
Marie Péju <mariepeju@MacBook-Pro-de-Marie.local>
2025-03-28 12:53:48 +0100

    feat: clean document_denied analytics

--- models/analytics/analytics_daily_checked_options.sql
+++ models/analytics/analytics_daily_checked_options.sql
@@ -1,15 +1,17 @@
 select
-    document_category
+    id
+    , document_category
     , document_sub_category
     , checked_options_id
     , checked_options
     , document_user_type
     , tenant_origin
     , DATE(document_denied_at) as document_denied_at
-    , COUNT(*) as nb_checked_options
+    , COUNT(distinct checked_options_id) as nb_checked_options
 from {{ ref('core_document_denied') }}
 group by
     DATE(document_denied_at)
+    , id
     , document_category
     , document_sub_category
     , checked_options_id
--- models/core/document/core_document_denied.sql
+++ models/core/document/core_document_denied.sql
@@ -1,5 +1,5 @@
 select
-    staging_document_denied_reasons.id
+    staging_document_denied_options.id
     , staging_document_denied_reasons.checked_options
     , staging_document_denied_reasons.checked_options_id
     , staging_document_denied_reasons.comment
@@ -10,16 +10,17 @@ select
     , staging_document_denied_options.document_user_type
     , staging_document_denied_options.code
     , staging_document.id as document_id
-    , staging_document.document_category
-    , staging_document.document_sub_category
+    , core_document_reference.document_category
+    , staging_document_denied_options.document_sub_category
     , staging_document.created_at as document_created_at
     , staging_document.tenant_id
     , staging_document.guarantor_id
-    , case when staging_document.tenant_id is not null then 'TENANT' else 'GUARANTOR' end as user_type
     , core_tenant_account.tenant_origin
-from {{ ref('staging_document_denied_reasons') }} as staging_document_denied_reasons
-left join {{ ref('staging_document_denied_options') }} as staging_document_denied_options on staging_document_denied_options.id = staging_document_denied_reasons.checked_options_id
-left join {{ ref('staging_document') }} as staging_document on staging_document.id = staging_document_denied_reasons.document_id
+    , case when staging_document.tenant_id is not null then 'TENANT' else 'GUARANTOR' end as user_type
+from {{ ref('staging_document_denied_options') }} as staging_document_denied_options
+left join {{ ref('staging_document_denied_reasons') }} as staging_document_denied_reasons on staging_document_denied_options.id = staging_document_denied_reasons.checked_options_id
+left join {{ ref('core_document_reference') }} as core_document_reference on staging_document_denied_options.document_sub_category = core_document_reference.document_sub_category
+left join {{ ref('staging_document') }} as staging_document on staging_document_denied_reasons.document_id = staging_document.id
 left join {{ ref('core_tenant_account') }} as core_tenant_account on staging_document.tenant_id = core_tenant_account.id
 
 /*
