4e15b59fad6fb8c4198d099322a6b6698ad228bb
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-03 15:02:09 +0200

    fix: lint issues

--- models/staging/tenant/staging_tenant.sql
+++ models/staging/tenant/staging_tenant.sql
@@ -2,10 +2,10 @@ select
     CAST(id as INTEGER)
     , CAST(tenant_type as VARCHAR)
     , CAST(apartment_sharing_id as INTEGER)
-    , CASE 
-        WHEN left(zip_code, 5) ~ '^[0-9]{5}$' THEN left(zip_code, 5)
-        ELSE 'invalid'
-    END AS zip_code
+    , case
+        when LEFT(zip_code, 5) ~ '^[0-9]{5}$' then LEFT(zip_code, 5)
+        else 'invalid'
+    end as zip_code
     , CAST(honor_declaration as BOOLEAN)
     , CAST(last_update_date as TIMESTAMP) as last_updated_at
     , CAST(COALESCE(status, 'INCOMPLETE') as VARCHAR) as status
