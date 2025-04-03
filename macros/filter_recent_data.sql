d3d1abf9022a33108865f8b12996b524f5eb7742
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-03 15:01:13 +0200

    fix: columns missing and data cleaning in source

--- macros/filter_recent_data.sql
+++ macros/filter_recent_data.sql
@@ -1,6 +1,6 @@
 {% macro filter_recent_data(date_field) %}
     {% if target.name != 'prod' %}
-        WHERE {{ date_field }} >= current_date - interval '8 week'
+        WHERE {{ date_field }} >= '2025-01-01 00:00:00'
     {% else %}
         WHERE {{ date_field }} >= '2023-01-01 00:00:00'
     {% endif %}
