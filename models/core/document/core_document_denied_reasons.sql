131cccac8af5f13a53c424801c761f8220434b94
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-07 16:06:25 +0200

    fix: analytics model

--- models/core/document/core_document_denied_reasons.sql
+++ models/core/document/core_document_denied_reasons.sql
@@ -10,22 +10,23 @@ select
     , staging_document_denied_options.denied_options_value
 
     , staging_document_denied_options.document_sub_category as denied_options_sub_category
-    , staging_document_denied_options.code
+    , staging_document_denied_options.code as denied_options_code
     , staging_document_denied_reasons.operator_comment
-    , staging_document.tenant_id
 
+    , staging_document.tenant_id
     , staging_document.guarantor_id
+
     , case
         when staging_document.tenant_id is not null then 'TENANT'
         else 'GUARANTOR'
     end as document_tenant_type
-
-    --, core_tenant_account.tenant_origin
 
 from {{ ref('staging_document_denied_reasons') }} as staging_document_denied_reasons
 left join {{ ref('staging_document') }} as staging_document
     on staging_document_denied_reasons.document_id = staging_document.id
 left join {{ ref('staging_document_denied_options') }} as staging_document_denied_options
     on staging_document_denied_reasons.denied_options_id = staging_document_denied_options.id
--- left join {{ ref('core_tenant_account') }} as core_tenant_account
---     on staging_document.tenant_id = core_tenant_account.id
+
+-- BUG: il n'y a visiblement un bug lors de la suppression des documents Garants (pas de DELETE CASCADE ?)
+-- Certain Ids de documents dans staging_document_denied_reasons n'existent pas dans staging_document
+where staging_document.id is not null
