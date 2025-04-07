fb534eb74f247e42f9909a305e23d46f1b46cd7c
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-07 17:27:24 +0200

    refactor: remove useless code

--- models/analytics/analytics_weekly_operations.sql
+++
@@ -1,8 +0,0 @@
-select
-    DATE_TRUNC('week', created_at) as created_date
-    , SUM(case when log_type in ('ACCOUNT_DENIED', 'ACCOUNT_VALIDATED') then 1 else 0 end) as nb_operation
-    , SUM(case when log_type = 'ACCOUNT_DENIED' then 1 else 0 end) as nb_denied
-    , SUM(case when log_type = 'ACCOUNT_VALIDATED' then 1 else 0 end) as nb_validation
-from {{ ref('core_tenant_log') }}
-group by
-    DATE_TRUNC('week', created_at)
