be86d0ec52c8f839215b16988b063bd0a0b6a83b
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-10 17:56:14 +0200

    fix: lint errors

--- models/analytics/analytics_daily_operations.sql
+++ models/analytics/analytics_daily_operations.sql
@@ -1,8 +1,8 @@
 select
-    DATE(created_at) as operation_date
-    , operator_id
+    operator_id
     , operator_name
     , operator_email
+    , DATE(created_at) as operation_date
     , SUM(case when log_type in ('ACCOUNT_DENIED', 'ACCOUNT_VALIDATED') then 1 else 0 end) as nb_operation
     , SUM(case when log_type = 'ACCOUNT_DENIED' then 1 else 0 end) as nb_denied
     , SUM(case when log_type = 'ACCOUNT_VALIDATED' then 1 else 0 end) as nb_validation
