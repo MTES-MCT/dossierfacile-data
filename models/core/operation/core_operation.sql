85a896eccc900bf50d7e73ebde47c331606fa343
Romaric <Rhmaric@users.noreply.github.com>
2025-04-11 12:34:22 +0200

    feat: add validation and denial flags to core_operation model (#22)

--- models/core/operation/core_operation.sql
+++ models/core/operation/core_operation.sql
@@ -5,6 +5,8 @@ select
     , user_operator.name as operator_name
     , operation.log_type
     , operation.created_at
+    , case when log_type = 'ACCOUNT_VALIDATED' then 1 else 0 end as validation_flag
+    , case when log_type = 'ACCOUNT_DENIED' then 1 else 0 end as denial_flag
 from {{ ref('staging_tenant_log') }} as operation
 left join {{ ref('staging_operator') }} as user_operator
     on operation.operator_id = user_operator.id
