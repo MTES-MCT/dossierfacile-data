19252fecb030804f874816afbf344a83f369f26d
Romaric <Rhmaric@users.noreply.github.com>
2025-04-15 17:53:02 +0200

    rework: rework core_document_denied_reasons (#24)

--- models/analytics/analytics_daily_denial_reasons.sql
+++ models/analytics/analytics_daily_denial_reasons.sql
@@ -1,22 +1,18 @@
 select
     denied_option_id
     , denied_option_value
-    , denied_option_sub_category
     , denied_option_code
     , document_category
     , document_sub_category
-    , document_category_step
     , document_tenant_type
     , DATE(document_denied_at) as denial_date
-    , COUNT(document_id) as nb_denied_documents
+    , COUNT(unique_id) as nb_denial_reasons
 from {{ ref('core_document_denied_reasons') }}
 group by
     DATE(document_denied_at)
     , denied_option_id
     , denied_option_value
-    , denied_option_sub_category
     , denied_option_code
     , document_category
     , document_sub_category
-    , document_category_step
     , document_tenant_type
