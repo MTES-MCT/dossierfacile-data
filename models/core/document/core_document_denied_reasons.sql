74eb86444dd93c336db212d11b438fe20d26e627
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-15 17:48:51 +0200

    rework: rework core_document_denied_reasons

--- models/core/document/core_document_denied_reasons.sql
+++ models/core/document/core_document_denied_reasons.sql
@@ -1,32 +1,18 @@
 select
-    staging_document.id as document_id
-    , staging_document.document_category
-    , staging_document.document_sub_category
-    , staging_document.document_category_step
-    , staging_document.created_at as document_created_at
+    {{ dbt_utils.generate_surrogate_key(['staging_document_denied_reasons.id', 'staging_document_denied_options.code']) }} as unique_id
     , staging_document_denied_reasons.created_at as document_denied_at
 
     , staging_document_denied_reasons.denied_option_id
     , staging_document_denied_reasons.denied_option_value
 
-    , staging_document_denied_options.document_sub_category as denied_option_sub_category
+    , staging_document_category.document_category
+    , staging_document_category.document_sub_category
+    , staging_document_denied_options.document_tenant_type
     , staging_document_denied_options.code as denied_option_code
     , staging_document_denied_reasons.operator_comment
-
-    , staging_document.tenant_id
-    , staging_document.guarantor_id
-
-    , case
-        when staging_document.tenant_id is not null then 'TENANT'
-        else 'GUARANTOR'
-    end as document_tenant_type
-
 from {{ ref('staging_document_denied_reasons') }} as staging_document_denied_reasons
-left join {{ ref('staging_document') }} as staging_document
-    on staging_document_denied_reasons.document_id = staging_document.id
 left join {{ ref('staging_document_denied_options') }} as staging_document_denied_options
     on staging_document_denied_reasons.denied_option_id = staging_document_denied_options.id
-
--- BUG: il n'y a visiblement un bug lors de la suppression des documents Garants (pas de DELETE CASCADE ?)
--- Certain Ids de documents dans staging_document_denied_reasons n'existent pas dans staging_document
-where staging_document.id is not null
+left join {{ ref('staging_document_category') }} as staging_document_category
+    on staging_document_denied_options.document_sub_category = staging_document_category.document_sub_category
+where staging_document_category.document_category is not null
--- models/staging/document/staging_document_denied_options.sql
+++ models/staging/document/staging_document_denied_options.sql
@@ -2,6 +2,6 @@ select
     CAST(id as INTEGER)
     , CAST(message_value as VARCHAR) as denied_option_value
     , CAST(document_sub_category as VARCHAR)
-    , CAST(document_user_type as VARCHAR) as document_tenant_type
+    , UPPER(CAST(document_user_type as VARCHAR)) as document_tenant_type
     , CAST(code as VARCHAR)
 from {{ source('dossierfacile', 'document_denied_options') }}
