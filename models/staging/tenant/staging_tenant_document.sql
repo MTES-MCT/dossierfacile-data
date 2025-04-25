d73564249f501042a48a39dc880dfefdcd29770c
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-04-25 11:06:20 +0200

    chore: event-driven logic for staging_tenant_document

--- models/staging/tenant/staging_tenant_document.sql
+++ models/staging/tenant/staging_tenant_document.sql
@@ -1,124 +1,78 @@
-with pivot_document_table as (
+with tenant_document_events as (
     select
         id
-        , document_category
         , tenant_id
-        , modified_at
-        , case when document_status = 'VALIDATED' then monthly_net_income end as validated_net_income
-
-        , case when document_category = 'IDENTIFICATION' then document_status end as identification_document_status
-        , case when document_category = 'IDENTIFICATION' then document_sub_category end as identification_sub_category
-
-        , case when document_category = 'FINANCIAL' then document_status end as financial_document_status
-        , case when document_category = 'FINANCIAL' then document_sub_category end as financial_sub_category
-
-        , case when document_category = 'RESIDENCY' then document_status end as residency_document_status
-        , case when document_category = 'RESIDENCY' then document_sub_category end as residency_sub_category
-
-        , case when document_category = 'PROFESSIONAL' then document_status end as professional_document_status
-        , case when document_category = 'PROFESSIONAL' then document_sub_category end as professional_sub_category
-
-        , case when document_category = 'TAX' then document_status end as tax_document_status
-        , case when document_category = 'TAX' then document_sub_category end as tax_sub_category
-    from {{ ref('staging_document') }}
+        , created_at
+        , document_category
+        , document_sub_category
+    from {{ ref('staging_tenant_log') }}
+    -- log_type = 'ACCOUNT_EDITED' and edition_type = 'ADD_DOCUMENT'
+    where edition_type = 'ADD_DOCUMENT'
 )
 
-, last_status_document_table as (
+, last_tenant_document_status as (
     select distinct
         tenant_id
         , document_category
-        , SUM(validated_net_income) over (
+        , FIRST_VALUE(created_at) over (
             partition by tenant_id, document_category
-            order by modified_at asc
+            order by created_at asc
             rows between unbounded preceding and unbounded following
-        ) as sum_validated_net_income
+        ) as first_added_at
+        , LAST_VALUE(document_sub_category) over (
+            partition by tenant_id, document_category
+            order by created_at asc
+            rows between unbounded preceding and unbounded following
+        ) as last_document_sub_category
+    from tenant_document_events
+)
 
-        , LAST_VALUE(identification_document_status) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_identification_document_status
-        , LAST_VALUE(identification_sub_category) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_identification_sub_category
+, pivot_tenant_document_status as (
+    select
+        tenant_id
+        , case when document_category = 'IDENTIFICATION' then last_document_sub_category end as identification_last_sub_category
+        , case when document_category = 'IDENTIFICATION' then first_added_at end as identification_first_added_at
 
-        , LAST_VALUE(financial_document_status) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_financial_document_status
-        , LAST_VALUE(financial_sub_category) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_financial_sub_category
+        , case when document_category = 'FINANCIAL' then last_document_sub_category end as financial_last_sub_category
+        , case when document_category = 'FINANCIAL' then first_added_at end as financial_first_added_at
 
-        , LAST_VALUE(residency_document_status) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_residency_document_status
-        , LAST_VALUE(residency_sub_category) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_residency_sub_category
+        , case when document_category = 'RESIDENCY' then last_document_sub_category end as residency_last_sub_category
+        , case when document_category = 'RESIDENCY' then first_added_at end as residency_first_added_at
 
-        , LAST_VALUE(professional_document_status) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_professional_document_status
-        , LAST_VALUE(professional_sub_category) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_professional_sub_category
+        , case when document_category = 'PROFESSIONAL' then last_document_sub_category end as professional_last_sub_category
+        , case when document_category = 'PROFESSIONAL' then first_added_at end as professional_first_added_at
 
-        , LAST_VALUE(tax_document_status) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_tax_document_status
-        , LAST_VALUE(tax_sub_category) over (
-            partition by tenant_id, document_category
-            order by modified_at asc
-            rows between unbounded preceding and unbounded following
-        ) as last_tax_sub_category
-
-    from pivot_document_table
+        , case when document_category = 'TAX' then last_document_sub_category end as tax_last_sub_category
+        , case when document_category = 'TAX' then first_added_at end as tax_first_added_at
+    from last_tenant_document_status
 )
 
 , tenant_document_table as (
     select
         tenant_id
-        , MAX(sum_validated_net_income) as validated_net_income
+        , MAX(identification_first_added_at) as identification_first_added_at
+        , MAX(identification_last_sub_category) as identification_last_sub_category
 
-        , MAX(last_identification_document_status) as identification_document_status
-        , MAX(last_identification_sub_category) as identification_sub_category
+        , MAX(financial_first_added_at) as financial_first_added_at
+        , MAX(financial_last_sub_category) as financial_last_sub_category
 
-        , MAX(last_financial_document_status) as financial_document_status
-        , MAX(last_financial_sub_category) as financial_sub_category
+        , MAX(residency_first_added_at) as residency_first_added_at
+        , MAX(residency_last_sub_category) as residency_last_sub_category
 
-        , MAX(last_residency_document_status) as residency_document_status
-        , MAX(last_residency_sub_category) as residency_sub_category
+        , MAX(professional_first_added_at) as professional_first_added_at
+        , MAX(professional_last_sub_category) as professional_last_sub_category
 
-        , MAX(last_professional_document_status) as professional_document_status
-        , MAX(last_professional_sub_category) as professional_sub_category
-
-        , MAX(last_tax_document_status) as tax_document_status
-        , MAX(last_tax_sub_category) as tax_sub_category
-    from last_status_document_table
+        , MAX(tax_first_added_at) as tax_first_added_at
+        , MAX(tax_last_sub_category) as tax_last_sub_category
+    from pivot_tenant_document_status
     group by tenant_id
 )
 
 select
     *
-    , case when identification_document_status is null then 0 else 1 end as has_identification_document
-    , case when financial_document_status is null then 0 else 1 end as has_financial_document
-    , case when residency_document_status is null then 0 else 1 end as has_residency_document
-    , case when professional_document_status is null then 0 else 1 end as has_professional_document
-    , case when tax_document_status is null then 0 else 1 end as has_tax_document
+    , case when identification_first_added_at is null then 0 else 1 end as has_identification_document
+    , case when financial_first_added_at is null then 0 else 1 end as has_financial_document
+    , case when residency_first_added_at is null then 0 else 1 end as has_residency_document
+    , case when professional_first_added_at is null then 0 else 1 end as has_professional_document
+    , case when tax_first_added_at is null then 0 else 1 end as has_tax_document
 from tenant_document_table
--- models/staging/tenant/staging_tenant_log.sql
+++ models/staging/tenant/staging_tenant_log.sql
@@ -9,6 +9,16 @@ with casting_log as (
         -- utile si on veut faire qq chose de plus fin comme par exemple dupliquer le dernier logs de suppression en un log d’archivage et de suppression
         -- il y avait un bug précédemment qui ne permettait pas de distinguer la suppression quand un archivage avait eu lieu.
         , CAST(migrate as BOOLEAN)
+        -- When the log type is ACCOUNT_EDITED and the log details are not null, we extract the edition type, document category and sub category from the log details
+        , case
+            when log_type = 'ACCOUNT_EDITED' and log_details is not null then log_details ->> 'editionType' || '_DOCUMENT'
+        end as edition_type
+        , case
+            when log_type = 'ACCOUNT_EDITED' and log_details is not null then log_details ->> 'documentCategory'
+        end as document_category
+        , case
+            when log_type = 'ACCOUNT_EDITED' and log_details is not null then log_details ->> 'documentSubCategory'
+        end as document_sub_category
     from {{ source('dossierfacile', 'tenant_log') }}
     {{ filter_recent_data('creation_date') }}
 )
@@ -20,4 +30,7 @@ select
     , casting_log.log_type
     , casting_log.created_at
     , casting_log.migrate
+    , casting_log.edition_type
+    , casting_log.document_category
+    , casting_log.document_sub_category
 from casting_log
