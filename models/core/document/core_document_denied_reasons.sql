a4e2ff01f8520431c180f01bde67651094070919
Romaric <Rhmaric@users.noreply.github.com>
2025-05-21 18:04:09 +0200

    refactor: denied_reasons models (staging and core) (#33)
    - add column document_id
    - remove multiple join
    - remove useless model document_category

--- models/core/document/core_document_denied_reasons.sql
+++ models/core/document/core_document_denied_reasons.sql
@@ -1,18 +1,14 @@
 select
-    {{ dbt_utils.generate_surrogate_key(['staging_document_denied_reasons.id', 'staging_document_denied_options.code']) }} as unique_id
-    , staging_document_denied_reasons.created_at as document_denied_at
+    {{ dbt_utils.generate_surrogate_key(['id', 'denied_option_id']) }} as unique_id
+    , created_at as document_denied_at
+    , document_id
+    , denied_option_id
+    , denied_option_value
 
-    , staging_document_denied_reasons.denied_option_id
-    , staging_document_denied_reasons.denied_option_value
+    , document_category
+    , document_sub_category
+    , document_category_step
+    , document_tenant_type
 
-    , staging_document_category.document_category
-    , staging_document_category.document_sub_category
-    , staging_document_denied_options.document_tenant_type
-    , staging_document_denied_options.code as denied_option_code
-    , staging_document_denied_reasons.operator_comment
-from {{ ref('staging_document_denied_reasons') }} as staging_document_denied_reasons
-left join {{ ref('staging_document_denied_options') }} as staging_document_denied_options
-    on staging_document_denied_reasons.denied_option_id = staging_document_denied_options.id
-left join {{ ref('staging_document_category') }} as staging_document_category
-    on staging_document_denied_options.document_sub_category = staging_document_category.document_sub_category
-where staging_document_category.document_category is not null
+    , operator_comment
+from {{ ref('staging_document_denied_reasons') }}
