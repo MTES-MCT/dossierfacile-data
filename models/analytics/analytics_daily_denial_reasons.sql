173393f9819988e819a3729f60f0cce680d45a6a
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-06-10 12:52:13 +0200

    fix: remove column denial_code, rename column owner_type

--- models/analytics/analytics_daily_denial_reasons.sql
+++ models/analytics/analytics_daily_denial_reasons.sql
@@ -1,9 +1,9 @@
 select
     denied_option_id
     , denied_option_value
-    , denied_option_code
     , document_category
     , document_sub_category
+    , document_category_step
     , document_tenant_type
     , DATE(document_denied_at) as denial_date
     , COUNT(unique_id) as nb_denial_reasons
@@ -12,7 +12,7 @@ group by
     DATE(document_denied_at)
     , denied_option_id
     , denied_option_value
-    , denied_option_code
     , document_category
     , document_sub_category
+    , document_category_step
     , document_tenant_type
