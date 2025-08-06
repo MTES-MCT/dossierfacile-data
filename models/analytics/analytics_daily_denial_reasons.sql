e1c36247b3470f58b789ab4daba948cb2d903f05
Romaric <Rhmaric@users.noreply.github.com>
2025-08-06 12:46:28 +0200

    fix: revert previous changes (#41)

--- models/analytics/analytics_daily_denial_reasons.sql
+++ models/analytics/analytics_daily_denial_reasons.sql
@@ -1,36 +1,18 @@
-with daily_denial_reasons as (
-    select
-        denied_option_id
-        , denied_option_value
-        , document_category
-        , document_sub_category
-        , document_category_step
-        , document_tenant_type
-        , DATE(document_denied_at) as denial_date
-        , COUNT(unique_id) as nb_denial_reasons
-    from {{ ref('core_document_denied_reasons') }}
-    group by
-        DATE(document_denied_at)
-        , denied_option_id
-        , denied_option_value
-        , document_category
-        , document_sub_category
-        , document_category_step
-        , document_tenant_type
-)
-
-, daily_operations as (
-    select
-        DATE(operation_hour) as operation_date
-        , SUM(nb_denied) as nb_denied
-    from {{ ref('analytics_hourly_operations') }}
-    group by
-        DATE(operation_hour)
-)
-
 select
-    daily_denial_reasons.*
-    , daily_operations.nb_denied
-from daily_denial_reasons
-left join daily_operations
-    on daily_denial_reasons.denial_date = daily_operations.operation_date
+    denied_option_id
+    , denied_option_value
+    , document_category
+    , document_sub_category
+    , document_category_step
+    , document_tenant_type
+    , DATE(document_denied_at) as denial_date
+    , COUNT(unique_id) as nb_denial_reasons
+from {{ ref('core_document_denied_reasons') }}
+group by
+    DATE(document_denied_at)
+    , denied_option_id
+    , denied_option_value
+    , document_category
+    , document_sub_category
+    , document_category_step
+    , document_tenant_type
