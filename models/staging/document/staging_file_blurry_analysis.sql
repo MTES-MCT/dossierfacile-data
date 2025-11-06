c41ab0fc2621042a6b1cac73ed615a9acb15ab8c
Romaric <Rhmaric@users.noreply.github.com>
2025-11-06 09:41:25 +0100

    feat: change laplacianVariance by OCR results (#50)

--- models/staging/document/staging_file_blurry_analysis.sql
+++ models/staging/document/staging_file_blurry_analysis.sql
@@ -6,7 +6,7 @@ with blurry_analysis_parsed as (
         , CAST(analysis_status as VARCHAR) as analysis_status
         , CAST(blurry_results as JSONB) as blurry_results_json
     from {{ source('dossierfacile', 'blurry_file_analysis') }}
-    where id > 6273288 -- blurry detection has been enabled on 2025-08-29 00:00:00
+    where id > 8052310 -- new blurry detection logic based on OCR has been enabled on 2025-10-30 00:00:00
 )
 
 , blurry_results_flattened as (
@@ -17,8 +17,8 @@ with blurry_analysis_parsed as (
         , analysis_status
         , blurry_results_json ->> 'isBlank' as is_blank
         , blurry_results_json ->> 'isBlurry' as is_blurry
-        , blurry_results_json ->> 'isReadable' as is_readable
-        , CAST(blurry_results_json ->> 'laplacianVariance' as DECIMAL(15, 2)) as laplacian_variance
+        , CAST(blurry_results_json ->> 'ocrTokens' as INTEGER) as ocr_token_count
+        , CAST(blurry_results_json ->> 'ocrMeanScore' as DECIMAL(5, 2)) as ocr_mean_score
     from blurry_analysis_parsed
 )
 
@@ -27,8 +27,8 @@ select
     , file_id
     , document_id
     , analysis_status
-    , laplacian_variance
+    , ocr_token_count
+    , ocr_mean_score
     , CAST(is_blank as BOOLEAN) as is_blank
     , CAST(is_blurry as BOOLEAN) as is_blurry
-    , CAST(is_readable as BOOLEAN) as is_readable
 from blurry_results_flattened
