a65c1227e8fcb36e2269e8558a8d447e9667a2bf
Romaric <Rhmaric@users.noreply.github.com>
2026-04-29 21:37:27 +0200

    refactor: build document model from tenant log (#66)
    keep track of deleted documents

--- models/core/document/core_document.sql
+++ models/core/document/core_document.sql
@@ -1,2 +1,26 @@
-select *
-from {{ ref('staging_document') }}
+select
+    staging_document_status.document_id
+    , staging_document_status.tenant_id
+    , staging_document_status.guarantor_id
+
+    , staging_document_status.document_category
+    , staging_document_status.document_sub_category
+    , case
+        when staging_document.id is null then 'UNKNOWN'
+        else staging_document.document_category_step
+    end as document_category_step
+
+    , COALESCE(staging_document.document_status, staging_document_status.document_status) as document_status
+
+    , staging_document.monthly_net_income
+
+    , staging_document.created_at
+    , staging_document.modified_at
+    , staging_document_status.added_at
+    , staging_document_status.deleted_at
+
+    , staging_document_status.total_add_document_events
+    , staging_document_status.total_delete_document_events
+from {{ ref('staging_document_status') }} as staging_document_status
+left join {{ ref('staging_document') }} as staging_document
+    on staging_document_status.document_id = staging_document.id
